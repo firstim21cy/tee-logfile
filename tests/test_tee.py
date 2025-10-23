a8c32cdee1e60d1df804d01a16f017d42d48b792
cheny <cheny@fcc.ca>
2025-10-23 18:28:48 -0400

    add all

--- tests/test_tee.py
+++ tests/test_tee.py
@@ -5,29 +5,46 @@ from tee_logfile.tee import Tee
 
 ROOT_PATH = Path(__file__).resolve().parent.parent
 
+import pytest
+from pathlib import Path
+import shutil
 
-def test_tee_str():
-    temp_dir = ROOT_PATH / 'tests' / 'temp'
-    log_file = temp_dir / 'test.log'
+@pytest.fixture
+def temp_dir():
+    path = ROOT_PATH / 'tests' / 'temp'
+    # path.mkdir(exist_ok=True)
+    yield path
+    if path.exists():
+        shutil.rmtree(path)
+
+@pytest.fixture
+def log_file(temp_dir):
+    file_path = temp_dir / "test.log"
+    yield file_path
+    if file_path.exists():
+        file_path.unlink()
+
+def test_tee_str(log_file):
+
     with Tee.context(str(log_file)):
         print('This is a test message.')
         print('Another line of output.')
 
-    assert True
+    content = log_file.read_text()
+    assert "Another line of output." in content
 
 
-def test_tee():
-    temp_dir = ROOT_PATH / 'tests' / 'temp'
-    log_file = temp_dir / 'test.log'
+def test_tee(log_file):
+
     with Tee.context(log_file.resolve()):
         print('This is a test message.')
         print('Another line of output.')
 
-    assert True
+    content = log_file.read_text()
+    assert "Another line of output." in content
 
 
-def test_tee_rm():
-    temp_dir = ROOT_PATH / 'tests' / 'temp'
+def test_tee_rm(temp_dir):
     log_file = temp_dir / 'test.log'
 
     if temp_dir.exists():
@@ -37,4 +54,5 @@ def test_tee_rm():
         print('This is a test message.')
         print('Another line of output.')
 
-    assert True
+    content = log_file.read_text()
+    assert "Another line of output." in content
