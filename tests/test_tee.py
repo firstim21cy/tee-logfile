c8a750ecc91334d6f40eb2ad29ac17de9a42541c
cheny <cheny@fcc.ca>
2025-10-23 18:10:08 -0400

    add all

--- tests/test_tee.py
+++ tests/test_tee.py
@@ -1,8 +1,39 @@
+import shutil
+from pathlib import Path
+
 from tee_logfile.tee import Tee
+
+ROOT_PATH = Path(__file__).resolve().parent.parent
+
+
+def test_tee_str():
+    temp_dir = ROOT_PATH / 'tests' / 'temp'
+    log_file = temp_dir / 'test.log'
+    with Tee.context(str(log_file)):
+        print('This is a test message.')
+        print('Another line of output.')
+
+    assert True
 
 
 def test_tee():
-    with Tee.context('temp/test_output.log'):
+    temp_dir = ROOT_PATH / 'tests' / 'temp'
+    log_file = temp_dir / 'test.log'
+    with Tee.context(log_file.resolve()):
+        print('This is a test message.')
+        print('Another line of output.')
+
+    assert True
+
+
+def test_tee_rm():
+    temp_dir = ROOT_PATH / 'tests' / 'temp'
+    log_file = temp_dir / 'test.log'
+
+    if temp_dir.exists():
+        shutil.rmtree(temp_dir)
+
+    with Tee.context(log_file):
         print('This is a test message.')
         print('Another line of output.')
 
